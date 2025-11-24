9ebdb5007c72534c1045e30fa9461941c2cfe467
AdilY04 <adil.y04@outlook.com>
2025-11-24 23:54:41 +0000

    V2

--- test.py
+++ test.py
@@ -2,5 +2,5 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
 
-df = pd.read_csv('datasets/perfume.Brainfunc.csv')
-print(df.head())
+df = pd.read_csv('datasets/perfume_Brainfunc.csv')
+print(df.head(5))
