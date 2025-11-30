24e1e0f921c7a91fcbf32f36be444e7a88c315e1
AdilY04 <adil.y04@outlook.com>
2025-11-30 00:24:58 +0000

    decision tree ajwa mejdool

--- test.py
+++ test.py
@@ -1,6 +1,21 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
+from sklearn import tree
+from sklearn.tree import DecisionTreeClassifier
 
-df = pd.read_csv('datasets/perfume_Brainfunc.csv')
-print(df.head(5))
+df = pd.read_csv('datasets/ajwa+or+medjool/AjwaOrMejdool.csv', sep=';')
+#print(df.head(5))
+#print(df.columns)
+d = {'Black': 0, 'Brown': 1}
+df['Color'] = df['Color'].map(d)
+
+features = ['Date Length (cm)', 'Date Diameter (cm)', 'Date Weight (g)',
+            'Pit Length (cm)', 'Calories (Kcal)', 'Color']
+x = df[features]
+y = df['Class (Ajwa or Medjool)']
+
+dtree = DecisionTreeClassifier()
+dtree = dtree.fit(x, y)
+tree.plot_tree(dtree, feature_names=features)
+plt.show()
